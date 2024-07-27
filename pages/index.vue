bff4cdc814ae0e930def85acf829b6eeb358d085
Bahng Ji Hoon <wpxm2003@naver.com>
2024-07-27 11:28:53 +0900

    nuxt base websocket add

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,42 @@
-<template>
-    <div>
-        <h1>안녕ㅎㅎㅎ</h1>
-    </div>
-</template>
-
 <script setup lang="ts">
 
+import { useWebSocket } from '@vueuse/core'
+
+const localUrl = 'ws://localhost:3000/api/websocket';
+const { status, data, send, open, close } = useWebSocket(localUrl)
+
+const message = ref('')
+const history = ref<string[]>([])
+
+watch(data, (receiveMsg: string) => {
+  history.value.push(`server: ${receiveMsg}`)
+})
+
+function sendMessage() {
+  if (message.value == null || message.value === '') {
+    return false
+  }
+
+  history.value.push(`client : ${message.value}`)
+  send(message.value);
+  message.value = '';
+}
+
 </script>
+
+<template>
+  <div>
+    <h1>WebSocket - let's go</h1>
+    <form @submit.prevent="sendMessage">
+      <input v-model="message"/>
+      <button type="submit">Send</button>
+    </form>
+    <div>
+      <p v-for="entry in history">{{ entry }}</p>
+    </div>
+  </div>
+</template>
+
 
 <style scoped>
 
